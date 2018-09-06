0693eed9621c322073aa52574b1bb1f223bc8a1b
mr ranvijay <ranvijay.cse@outlook.com>
2018-09-06 18:54:34 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { HousesComponent } from './houses/houses.component';
+import { CharactersComponent } from './characters/characters.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    HousesComponent,
+    CharactersComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
